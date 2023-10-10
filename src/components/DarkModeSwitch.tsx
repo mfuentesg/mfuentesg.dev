debbe9d2337f602626cd1671b2c63bc8f6f76b23
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2023-10-10 19:30:40 -0300

    initial commit

---
+++ src/components/DarkModeSwitch.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { MoonIcon, SunIcon } from 'lucide-react';
+import { motion } from 'framer-motion';
+
+const variants = {
+  dark: { rotate: -90, scale: 1 },
+  light: { rotate: 0, scale: 1 }
+};
+
+interface Props {
+  onClick: () => void;
+  className: string;
+  theme: 'dark' | 'light';
+}
+
+export default function Switch({ className = '', theme = 'dark', onClick }: Props) {
+  const [enabled, setEnabled] = useState(theme === 'dark');
+  const onClickHandler = (evt: React.MouseEvent<HTMLButtonElement>) => {
+    setEnabled((prevState) => !prevState);
+    const newTheme = enabled ? 'light' : 'dark';
+    if (onClick) {
+      onClick();
+    }
+
+    document.cookie = `theme=${newTheme}`;
+    document.documentElement.classList.remove('dark', 'light');
+    document.documentElement.classList.add(newTheme);
+  };
+
+  return (
+    <motion.button
+      initial={{ scale: 0 }}
+      variants={variants}
+      animate={enabled ? 'light' : 'dark'}
+      transition={{
+        type: 'spring',
+        stiffness: 460,
+        damping: 60
+      }}
+      className={className}
+      onClick={onClickHandler}
+    >
+      {enabled ? <SunIcon height="36" width="36" /> : <MoonIcon height="36" width="36" />}
+    </motion.button>
+  );
+}
